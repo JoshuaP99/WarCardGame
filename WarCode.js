a3e5400aea83dfd8be5bb673c35d9fedf6f7a13a
Joshua Pacheco <pikminguy56@gmail.com>
2022-05-12 14:26:10 -0600

    Code built still needs to be fixed to compare objects

--- WarCode.js
+++ WarCode.js
@@ -1,77 +1,86 @@
 const Suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
-const Values = ["A", "K", "Q", "J", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+const Values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+const Ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q","J", "A",]
+var player1 = []
+var player2 = []
 
-class Deck{
-  constructor(cards = freshDeck()){
-    this.cards = cards
+class Card {
+  constructor(value, rank, suit) {
+    this.value = value
+    this.rank = rank
+    this.suit = suit
   }
+}
 
+class Deck {
+  constructor(){
+    this.deck = []
+  }
+  
   get numberOfCards(){
-    return this.cards.length
+    return this.deck.length
   }
-
-  pop() {
-    return this.cards.shift()
-  }
-
-  push(card){
-    this.cards.push(card)
-  }
-
+  
   shuffle() {
     for (let i = this.numberOfCards -1; i > 0; i--) {
       const newIndex = Math.floor(Math.random() * (i + 1))
-      const oldValue = this.cards[newIndex]
-      this.cards[newIndex] = this.cards[i]
-      this.cards[i] = oldValue
+      const oldValue = this.deck[newIndex]
+      this.deck[newIndex] = this.deck[i]
+      this.deck[i] = oldValue
     }
   }
-}
   
-class Card {
-  constructor(suit, value) {
-    this.suit = suit
-    this.value = value
+  buildDeck(){
+    for (let suit in Suits) {
+      for (let i = 0; i < Ranks.length; i++) {
+        this.deck.push(new Card(Values[i], Ranks[i], Suits[suit]));
+      }
+    }
+  }
+
+  dealCard(){
+    return this.deck.pop()
   }
 }
 
-function freshDeck() {
-  return Suits.flatMap(suit => {
-    return Values.map(value => {
-      return new Card(suit, value)
-    })
-  })
+function startGame (){
+  let player1Score = 0
+  let player2Score = 0
+
+  freshDeck = new Deck()
+  freshDeck.buildDeck()
+  freshDeck.shuffle()
+
+  for (let i = 0; i < 26; i++) {
+    this.player1[i] = freshDeck.dealCard();
+    this.player2[i] = freshDeck.dealCard();
+  }
+
+  console.log(player1)
+  console.log(player2)
+  
+  for (let i = 0; i < 26; i++) {
+    if (player1[i] > player2[i]){
+      console.log(player1[i].values + "" + "is greater than" + "" + player2[i].values)
+      return player1Score + 1
+    } else if (player2[i] > player1[i]){
+      console.log(player2[i] + ""+ "is greater than" + "" + player1[i])
+      return player2Score + 1
+    } else {
+      console.log("draw")
+    }
+  }
+
+  console.log("Player 1's Score is " + player1Score)
+  console.log("Player 2's Score is " + player2Score)
+
+  if (player1Score > player2Score){
+    console.log("Player 1 wins with" + "" + player1Score)
+  } else if (player2Score > player1Score) {
+    console.log("Player 2 wins with" + "" + player2Score)
+  } else {
+    console.log("Game is a draw")
+  }
 }
 
-const cardValue = {
-  "2": 2,
-  "3": 3,
-  "4": 4,
-  "5": 5,
-  "6": 6,
-  "7": 7,
-  "8": 8,
-  "9": 9,
-  "10": 10,
-  J: 11,
-  Q: 12,
-  K: 13,
-  A: 14
-}
-
-let playerDeck
-let computerDeck
-let inRound
-let stop
-
-function startGame(){
-  deck = new Deck()
-  deck.Shuffle()
-
-  const deckMid = (deck.numberOfCards / 2)
-  Deck1 = new Deck(deck.cards.slive(0, deckMid))
-  Deck2 = new Deck(deck.cards.slice(deckMid, deck.numberOfCards))
-
-  console.log(Deck1)
-  console.log(Deck2)
-}
+startGame()
